cc7c3cc4201313ba357ca860debcda1ff7398465
Adam Jędryka <adam.jedryka98@gmail.com>
2021-08-20 15:19:23 +0200

    Rename serializer to plural word

--- backend/auth/views.py
+++ backend/auth/views.py
@@ -1,5 +1,5 @@
 from rest_framework.response import Response
-from auth.serializer import SocialAuthSerializer
+from auth.serializers import SocialAuthSerializer
 from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
 from allauth.socialaccount.providers.oauth2.client import OAuth2Client
 from dj_rest_auth.registration.views import SocialLoginView
